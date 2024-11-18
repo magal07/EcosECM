b80667f0a050ebf95f1018f43328ad342a087f50
Marcelo Magalhães <celoxd01@gmail.com>
2024-11-17 23:20:53 -0300

    Adicionar arquivos de projeto.

---
+++ ProductionOrderSEQUOR.API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductionOrderSEQUOR.API.Interfaces;
+using ProductionOrderSEQUOR.API.Models;
+using ProductionOrderSEQUOR.API.Repositories;
+
+namespace ProductionOrderSEQUOR.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // CRIANDO ROTA
+    public class UserController : Controller
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return Ok(await _userRepository.SelecionarTodos());
+        }
+        [HttpPost]
+        public async Task<ActionResult> CadastrarUser(User user)
+        {
+             _userRepository.Incluir(user);
+            if(await _userRepository.SavellAsync())
+            {
+                return Ok("Cliente cadastrado com sucesso!"); 
+            }
+            return BadRequest("Ocorreu um erro ao salvar o cliente.");
+        }
+    }
+}
---
+++ ProductionOrderSEQUOR.API/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProductionOrderSEQUOR.API.Models;
+
+namespace ProductionOrderSEQUOR.API.Interfaces
+{
+    public interface IUserRepository
+    {
+        // WEB - INCLUIR / ALTERAR E EXCLUIR
+
+        void Incluir(User user);
+        void Alterar(User user);
+        void Excluir(User user);
+        Task<User> SelecionarByPk(string Email);
+        Task<IEnumerable<User>> SelecionarTodos();
+
+        Task<bool> SavellAsync(); // mecanismo que vai salvar o método, sendo inclusão, alteração ou exclusão!
+
+    }
+}
